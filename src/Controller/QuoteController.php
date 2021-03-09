ed933994e61df117ca7242237e0f75e77ae3cf1b
debe0025 <jules.debeaumont@etudiant.univ-reims.fr>
2021-03-09 16:56:59 +0100

    Liste des citations

---
+++ src/Controller/QuoteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuoteController extends AbstractController
+{
+    /**
+     * @Route("/quotes", name="quote_index")
+     */
+    public function index(): Response
+    {
+
+        $quotes = [
+            [
+                'content' => 'Sire, Sire ! On en a gros !',
+                'meta' => 'Perceval, Livre II, Les Exploités'
+            ],[
+                'content' => 'Tout le monde est une pute, Grace. Nous vendons juste différentes parties de nous-mêmes.',
+                'meta' => 'Tommy Shelby'
+            ],[
+                'content' => 'Je viens de lui mettre une balle dans la tête…… Il m’a regardé de travers.',
+                'meta' => 'Tommy Shelby'
+            ],[
+                'content' => "2500 pièces d'or ???! Eh... eh... c'est un blague? 2500 pièces d'or, mais ou voulez vous que j'trouve 2500 pièces d'or, dans l'cul d'une vache ?!",
+                'meta' => 'Seigneur Jacca, Livre I, 21 : La taxe militaire'
+            ],[
+                'content' => "J’ai pénétré leur lieu d'habitation de façon subrogative […] en tapinant.",
+                'meta' => 'Hervé de Rinel, Livre III, 91 : L’Espion'
+            ]
+
+        ];
+
+
+        return $this->render('quote/index.html.twig', ['quotes' => $quotes]);
+    }
+}

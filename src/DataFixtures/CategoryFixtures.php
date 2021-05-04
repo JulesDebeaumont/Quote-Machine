789026d0dd52c63b559036ba383ab207caa6a8ed
debe0025 <jules.debeaumont@etudiant.univ-reims.fr>
2021-05-04 09:19:32 +0200

    Fix Vich + config api

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Factory\CategoryFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class CategoryFixtures extends Fixture
 {
@@ -19,7 +21,36 @@ class CategoryFixtures extends Fixture
         ), true);
 
         foreach ($categories as $category) {
-            CategoryFactory::new()->create($category);
+            $categoryEntity = CategoryFactory::new()->create($category);
+            if (isset($category['imageName'])) {
+                $this->setImage(
+                    $categoryEntity->object(),
+                    (
+                        __DIR__.
+                        DIRECTORY_SEPARATOR.
+                        'data'.
+                        DIRECTORY_SEPARATOR.
+                        'img'.
+                        DIRECTORY_SEPARATOR.
+                        $category['imageName']
+                    ),
+                    $category['imageName']);
+            }
         }
+        $manager->flush();
+    }
+
+    public function setImage(Category $category, string $path, string $filename)
+    {
+        $tmpPath = tempnam(sys_get_temp_dir(), 'categories_images');
+        copy($path, $tmpPath);
+
+        $category->setImageFile(new UploadedFile(
+            $tmpPath,
+            $filename,
+            'image/jpeg',
+            null,
+            true
+        ));
     }
 }
